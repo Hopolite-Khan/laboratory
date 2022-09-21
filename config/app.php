7000921d66bd1424b980e4f3567b99f3d69e563a
Reyu Hero <reyu443@gmail.com>
2022-09-21 04:31:29 +0430

    firebase removed

--- config/app.php
+++ config/app.php
@@ -176,7 +176,7 @@ return [
         App\Providers\RouteServiceProvider::class,
         Milon\Barcode\BarcodeServiceProvider::class,
         Barryvdh\DomPDF\ServiceProvider::class,
-        Kreait\Laravel\Firebase\ServiceProvider::class,
+        // Kreait\Laravel\Firebase\ServiceProvider::class,
         App\Providers\FirebaseServiceProvider::class,
     ],
 
@@ -235,7 +235,7 @@ return [
         'DNS1D' => Milon\Barcode\Facades\DNS1DFacade::class,
         'DNS2D' => Milon\Barcode\Facades\DNS2DFacade::class,
         'PDF' => Barryvdh\DomPDF\Facade::class,
-        
+
 
     ],
 
