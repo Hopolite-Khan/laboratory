f7d34f36ec5c0bbeb88dc97f690502aa2b065711
Reyu Hero <reyu443@gmail.com>
2022-09-22 14:49:46 +0430

    updated vite.config.js

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,13 @@
-import { defineConfig } from 'vite'
-import tailwindcss from 'tailwindcss'
-import autoprefixer from 'autoprefixer'
-import laravel from 'vite-plugin-laravel'
+import { defineConfig } from "vite";
+import tailwindcss from "tailwindcss";
+import autoprefixer from "autoprefixer";
+import laravel from "vite-plugin-laravel";
 
 export default defineConfig({
-	plugins: [
-		laravel({
-			postcss: [
-				autoprefixer(),
-			],
-		}),
-	],
-})
+    plugins: [
+        laravel({
+            input: ["/resources/sass/app.scss", "resources/scripts/main.ts"],
+            postcss: [autoprefixer()],
+        }),
+    ],
+});
