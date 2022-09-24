eac6a5b743caeb2265b28d4b788598597f60665b
Reyu Hero <reyu443@gmail.com>
2022-09-24 15:20:41 +0430

    vite prodution

--- resources/scripts/main.ts
+++ resources/scripts/main.ts
@@ -1,7 +1,5 @@
 // Build something cool. <3
-import logo from '@/resources/images/flask.png'
 import 'bootstrap'
 import axios from 'axios'
 import '../sass/app.scss'
 const test = 'test'
-document.getElementById('logo').src = logo
